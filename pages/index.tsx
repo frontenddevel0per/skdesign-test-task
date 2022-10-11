41ce680d55b1f66c1f1f378bf0b600782767fffc
Leonid <leonovozhilov1967@gmail.com>
2022-10-11 18:29:36 +0300

    layout: added all types of custom  button

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import CustomButton from "../components/button/button.component";
 const Home: NextPage = () => {
   return (
     <div className="wrapper">
-      <CustomButton>Пример текста</CustomButton>
+      <CustomButton>Simple button</CustomButton>
+      <CustomButton isDisabled>Disabled button</CustomButton>
+      <CustomButton isLoading>Loading button</CustomButton>
     </div>
   );
 };
